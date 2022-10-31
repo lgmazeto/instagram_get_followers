818e024833f234578c21a50c5dd6a1725e88c72e
Luiz Guilherme Mazeto <65667868+lgmazeto@users.noreply.github.com>
2022-10-30 23:19:31 -0300

    Building method getUserFollowers

--- main.py
+++ main.py
@@ -49,31 +49,47 @@ class InstagramBot():
 
     #em processo
    def getUserFollowers(self, username):
+    max = 20
     self.driver.get('https://www.instagram.com/' + username + '/')
-    followers = WebDriverWait(self.driver, 10).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'ul li a')))
-    followers.click()
-    sleep(20)
+    followers_btn = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'ul li a')))
+    followers_btn.click()
+    sleep(2)
+    popup = self.driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[2]')
+    #numberOfFollowersInList = len(popup.find_elements(By.CSS_SELECTOR, '_ab8w  _ab94 _ab97 _ab9f _ab9k _ab9p  _ab9- _aba8 _abcm'))
+    #print(numberOfFollowersInList)
+    
+    #_=0
+    #while (_ < max):
+    #        self.driver.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight", popup)
+    #        _ += 1
+    #        print(_)
+    #        sleep(2)
 
 
+    #followers = []
+    #for user in followersList.find_element(By.CSS_SELECTOR, 'li'):
+    #    userLink = user.find_element(By.CSS_SELECTOR, 'a').get_attribute('href')
+    #    print(userLink)
+    #    followers.append(userLink)
+    #    if (len(followers) == max):
+    #break
+
+   
    def closeBrowser(self):
     self.driver.close()
 
 
-   def __exit__(self, exc_type, exc_value, traceback):
+   def __exit__(self):
      self.closeBrowser()
 
 
 def main():
     print('ola mundo!')
-    driver = InstagramBot('', '')
+    driver = InstagramBot('processoszbn@gmail.com', '')
     driver.signIn()
-    
     #driver.followWithUsername('guilhermemazeto')
-    
     driver.getUserFollowers('guilhermemazeto')
-    
     #driver.unfollowWithUsername('guilhermemazeto')
-    
     driver.closeBrowser()
 
 
